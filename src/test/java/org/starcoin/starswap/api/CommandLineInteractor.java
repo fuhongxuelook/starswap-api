e59fd85a670a620e4948a6f9f7c397538da375cc
yangjiefeng <yangjiefeng@gmail.com>
2021-07-09 00:12:38 +0800

    增加测试代码。

---
+++ src/test/java/org/starcoin/starswap/api/CommandLineInteractor.java
@@ -0,0 +1,81 @@
+package org.starcoin.starswap.api;
+
+import java.io.*;
+
+public class CommandLineInteractor {
+
+    private Process process;
+
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
+
+    public CommandLineInteractor(Process process) {
+        this.reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        this.writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+    }
+
+    public CommandLineInteractor expect(String expectFor, int timeOutSeconds) {
+        boolean ok = false;
+        try {
+            ok = expect(this.reader, expectFor, timeOutSeconds);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        if (!ok) {
+            throw new RuntimeException("Expect for: " + expectFor + ", but failed.");
+        }
+        return this;
+    }
+
+    public CommandLineInteractor sendLine(String line) {
+        try {
+            sendLine(this.writer, line);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    private static void sendLine(BufferedWriter writer, String outputLine) throws IOException {
+        writer.write(outputLine);
+        writer.newLine();
+        writer.flush();
+    }
+
+    private static boolean expect(BufferedReader reader, String expectFor, int timeOutSeconds) throws IOException, InterruptedException {
+        long startTime = System.currentTimeMillis();
+        boolean expectOk = false;
+        StringBuilder inputSB = new StringBuilder();
+        while (true) {
+            String bufferStr = null;
+            while (reader.ready()) {
+                char[] charBuffer = new char[200];
+                int len = reader.read(charBuffer, 0, charBuffer.length);
+                bufferStr = new String(charBuffer, 0, len);
+                //System.out.println("----" + bufferStr);
+                System.out.print(bufferStr);
+                inputSB.append(bufferStr);
+            }
+            if (bufferStr == null) {
+                //System.out.println("=========== buffer str is null");
+                Thread.sleep(100);
+            }
+            String inputStr = inputSB.toString();
+            //System.out.println(inputStr);
+            if (inputStr.contains(expectFor)) {
+                expectOk = true;
+                break;
+            }
+            if (startTime + timeOutSeconds * 1000 < System.currentTimeMillis()) {
+                break;
+            }
+        }
+        return expectOk;
+    }
+}
--- src/test/java/org/starcoin/starswap/api/DevNetworkInteractApp.java
+++ src/test/java/org/starcoin/starswap/api/DevNetworkInteractApp.java
@@ -1,6 +1,7 @@
 package org.starcoin.starswap.api;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 
 public class DevNetworkInteractApp {
 
@@ -8,78 +9,58 @@ public class DevNetworkInteractApp {
         String shellPath = "/bin/sh";
         String starcoinCmd = "/Users/yangjiefeng/Documents/starcoinorg/starcoin/target/debug/starcoin -n dev -d alice console";
         String moveProjectDir = "/Users/yangjiefeng/Documents/starcoinorg/starswap-core";
-
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Please enter two account private keys");
+        }
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(
-                  new String[]{shellPath, "-c", starcoinCmd}
+                    new String[]{shellPath, "-c", starcoinCmd}
                     //new String[] {starcoinCmd, "-n", "dev", "console"}
             );
             processBuilder.directory(new File(moveProjectDir));
             //processBuilder.inheritIO();
             processBuilder.redirectErrorStream(true);
             Process process = processBuilder.start();
-            String expectFor = "Start console,";
+            CommandLineInteractor commandLineInteractor = new CommandLineInteractor(process);
+            String firstPrivateKey = args[0];
+            String secondPrivateKey = args[1];
+            commandLineInteractor.expect("Start console,", 10)
+                    // 导入账户，部署合约
+                    .sendLine("account import -i " + firstPrivateKey)
+                    .expect("\"ok\":", 10)
+                    .sendLine("account import -i " + secondPrivateKey)
+                    .expect("\"ok\":", 10)
+                    .sendLine("account default 0x07fa08a855753f0ff7292fdcbe871216")
+                    .expect("\"ok\":", 10)
+                    .sendLine("account unlock 0x07fa08a855753f0ff7292fdcbe871216")
+                    .expect("\"ok\":", 10)
+                    .sendLine("account unlock 0xff2794187d72cc3a9240198ca98ac7b6")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev get-coin 0x07fa08a855753f0ff7292fdcbe871216")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev get-coin 0xff2794187d72cc3a9240198ca98ac7b6")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev deploy storage/0x07fa08a855753f0ff7292fdcbe871216/modules/TokenSwap.mv -b")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev deploy storage/0x07fa08a855753f0ff7292fdcbe871216/modules/TokenSwapRouter.mv -b")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev deploy storage/0x07fa08a855753f0ff7292fdcbe871216/modules/TokenSwapScripts.mv -b")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev deploy storage/0x07fa08a855753f0ff7292fdcbe871216/modules/Bot.mv -b")
+                    .expect("\"ok\":", 10)
+                    .sendLine("dev deploy storage/0x07fa08a855753f0ff7292fdcbe871216/modules/Ddd.mv -b")
+                    .expect("\"ok\":", 10)
+            // 注册代币资源、发币
+            // todo
+            //.sendLine()
+            ;
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
 
-            boolean expectOk = expect(reader, expectFor, 10);
-            System.out.println("==============" + expectOk);
-            //if (expectOk) {
-                sendLine(writer,
-                        "account import -i xxxx");
-            //}
-            expectOk = expect(reader, "\"ok\":", 10);
-            System.out.println("==============" + expectOk);
-            sendLine(writer, "account default 0x07fa08a855753f0ff7292fdcbe871216");
-            expectOk = expect(reader, "\"ok\":", 10);
-            System.out.println("==============" + expectOk);
-            sendLine(writer, "dev get-coin 0x07fa08a855753f0ff7292fdcbe871216");
-            expectOk = expect(reader, "\"ok\":", 10);
-            System.out.println("==============" + expectOk);
-            sendLine(writer, "account show");
-            expectOk = expect(reader, "\"ok\":", 10);
-            System.out.println("==============" + expectOk);
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
 
     }
 
-    private static void sendLine(BufferedWriter writer, String outputLine) throws IOException {
-        writer.write(outputLine);
-        writer.newLine();
-        writer.flush();
-    }
-
-    private static boolean expect(BufferedReader reader, String expectFor, int timeOutSeconds) throws IOException, InterruptedException {
-        long startTime = System.currentTimeMillis();
-        boolean expectOk = false;
-        StringBuilder inputSB = new StringBuilder();
-        while (true) {
-            String bufferStr = null;
-            while (reader.ready()) {
-                char[] charBuffer = new char[200];
-                int len = reader.read(charBuffer, 0, charBuffer.length);
-                bufferStr = new String(charBuffer, 0, len);
-                System.out.println("----" + bufferStr);
-                inputSB.append(bufferStr);
-            }
-            if (bufferStr == null) {
-                //System.out.println("=========== buffer str is null");
-                Thread.sleep(100);
-            }
-            String inputStr = inputSB.toString();
-            //System.out.println(inputStr);
-            if (inputStr.contains(expectFor)) {
-                expectOk = true;
-                break;
-            }
-            if (startTime + timeOutSeconds * 1000 < System.currentTimeMillis()) {
-                break;
-            }
-        }
-        return expectOk;
-    }
 }
